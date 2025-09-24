cad11ff0b2d80f6ae33a0dd38de1bba75a133796
stefkovicsrichard <stefkovics.richard@ckik.hu>
2025-09-24 10:05:55 +0200

    rounlkik kezs

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const clock = document.getElementById("clock");
+var kep;
+var clicks = 0;
+var createdss = false;
+const kepek = ["assets/kata.jpg", "assets/richard.jpg"]
 
 function ora() {
   var ido = new Date().toLocaleString("hu-HU", {timeZone: "Europe/Budapest"});
@@ -10,4 +14,19 @@ setInterval(ora, 1000);
 function toggletext(e) {
   if (e.classList.contains("showing")) e.classList.remove("showing");
   else e.classList.add("showing");
+}
+
+function kepgen() {
+  if (createdss) {
+    kep.src = kepek[clicks];
+    if (clicks == kepek.length-1) clicks = 0;
+    else clicks++
+  } else {
+    var div = document.getElementById("slideshow");
+    kep = document.createElement("img");
+    kep.id = "kepek"
+    div.appendChild(kep);
+    createdss=true;
+    kepgen();
+  }
 }
