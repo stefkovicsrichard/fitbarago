634890121965c86e8abbfbc720f45f2d9700e8f3
stefkovicsrichard <stefkovics.richard@ckik.hu>
2025-09-29 07:54:51 +0200

    nagyon nem jo

--- todoscript2.js
+++ todoscript2.js
@@ -30,6 +30,11 @@ function downe(e) {
     }
 }
 
+function done(e) {
+	let index = tasks.indexOf(e);
+	tasks[index].innerText = "✓"
+}
+
 // --- Remove ---
 function dele(e) {
     tasks.splice(tasks.indexOf(e), 1);
@@ -71,16 +76,16 @@ function updateul() {
 		todos.innerHTML += `
 		<li class="item container">
 			<div class="row">
-				<div class="col-11">
+				<div class="col-xl-11 col-lg-10 col-md-9">
 					<h3>${e.name}</h3>
 					<p>${e.desc}</p>
 				</div>
-				<div class="col-1 container text-center">
+				<div class="col-xl-1 col-lg-2 col-md-3 container text-center">
 					<div class="row">
 						<div class="col-12"><a onclick="upe(tasks[${tasks.indexOf(e)}])">▲</a><a onclick="downe(tasks[${tasks.indexOf(e)}])">▼</a>
 					</div>
 					<div class="row">
-						<div class="col-12"><a onclick="done(this)">o✓</a></div>
+						<div class="col-12"><a onclick="done(tasks[${tasks.indexOf(e)}])">o</a></div>
 					</div>
 					<div class="row">
 						<div class="col-12"><a onclick="dele(tasks[${tasks.indexOf(e)}])">x</a>
